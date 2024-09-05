659ec4c44607d76ce520ba754963c225209078e3
Vidura Vijerathne <85375880+ViduraVijerathne@users.noreply.github.com>
2024-09-05 14:09:04 +0530

    move user controllers to controller.user

--- src/java/controllers/user/Verification.java
+++ src/java/controllers/user/Verification.java
@@ -2,12 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package controllers;
+package controllers.user;
 
-import com.google.gson.Gson;
-import config.MyGson;
+import dto.AuthDTO;
 import dto.ServiceResponse;
-import dto.UserDTO;
 import exceptions.ServiceException;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -16,39 +14,36 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import services.UserServices;
 
 /**
  *
  * @author vidur
  */
-@WebServlet(name = "Register", urlPatterns = {"/Register"})
-public class Register extends HttpServlet {
-    UserServices userServices;
+@WebServlet(name = "Verification", urlPatterns = {"/Verification"})
+public class Verification extends HttpServlet {
+
+    private UserServices userServices;
 
     @Override
     public void init() throws ServletException {
         userServices = new UserServices();
     }
-    
-    
-    
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("application/json");
-        Gson gson = MyGson.excludeFieldsWithoutExposeAnnotation();
-        UserDTO userDTO = gson.fromJson(req.getReader(),UserDTO.class);
-        
-        
-        try{
-            ServiceResponse response = userServices.register(userDTO);
+        AuthDTO auth = AuthDTO.fromRequest(req);
+        HttpSession httpSession = req.getSession();
+        try {
+            ServiceResponse response = userServices.verification(auth, httpSession);
             resp.getWriter().print(response.getData().toString());
             resp.setStatus(response.getStatusCode());
-        }catch(ServiceException ex){
+        } catch (ServiceException ex) {
             resp.setStatus(ex.getStatusCode());
             resp.getWriter().write(ex.getMessage());
         }
-//        resp.getWriter().write(userDTO.toString());
     }
 
 }
