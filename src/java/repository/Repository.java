440b5a13e501490598277d70a120c05e69d0a96e
Vidura Vijerathne <85375880+ViduraVijerathne@users.noreply.github.com>
2024-09-13 19:22:32 +0530

    optimized : repository save method

--- src/java/repository/Repository.java
+++ src/java/repository/Repository.java
@@ -5,23 +5,40 @@
 package repository;
 
 import entity.MyEntity;
+import lombok.Setter;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 /**
  *
  * @author vidur
  */
-
+@Setter
 abstract class Repository<E extends MyEntity> {
+
     Session session;
+    private boolean isHaveMainPrimatyKey = true;
 
     public Repository() {
         session = config.HibernateUtil.getSessionFactory().openSession();
     }
-    
-    protected Session getSession(){
+
+    protected Session getSession() {
         session = config.HibernateUtil.getSessionFactory().openSession();
         return session;
     }
-    
+
+    public E save(E entity) {
+//         session = getSession();
+        Transaction transaction = session.beginTransaction();
+        if (isHaveMainPrimatyKey) {
+            int id = (Integer) session.save(entity);
+            entity.setId(id);
+        } else {
+            session.save(entity);
+        }
+        transaction.commit();
+        return entity;
+    }
+
 }
